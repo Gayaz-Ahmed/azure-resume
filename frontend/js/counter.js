bffd885dcc2489baaa6a692356cb32b468ea083f
Gayaz-Ahmed <gayaz.ahmed99@gmail.com>
2021-06-24 22:14:38 +0530

    Created Azure Function api

--- frontend/js/counter.js
+++ frontend/js/counter.js
@@ -1,10 +1,10 @@
 
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
-})
+});
 
 
-const functionAPI = '';
+const functionAPI = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
@@ -13,9 +13,33 @@ const getVisitCount = () => {
     }).then(response => {
         console.log("Webiste called Function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
+        document.getElementById('counter').innerText = count;
     }).catch(function(error){
         console.log(error);
     });
     return count;
 }
+
+
+/*
+window.addEventListener('DOMContentLoaded', (event) => {
+    getCounter();
+});
+
+const azureFunctionApi = "https://cloudresumeazure.azurewebsites.net/api/resumecounter";
+
+const getCounter = async () => {
+    try {
+        const response = await fetch(azureFunctionApi);
+
+        const data = await response.json();
+
+        const element = document.getElementById('visitor-num');
+
+        element.textContent = `${data.counter} times`;
+              
+    } catch (error) {
+        console.log(error);
+    } 
+} 
+*/
